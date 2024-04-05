34e3d0da38b9c71c887d99e8b4889cd5c259b1d7
Irinaaissa <irinaalisa7506@gmail.com>
2024-04-05 15:26:43 +0300

    5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import PageTitle from "../components/PageTitle/PageTitle";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <PageTitle>
-        Task manager welcome page{" "}
-        <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span>
-        <div> </div>
-        <p>
-          This is a phone book. With which you can add contacts and delete
-          contacts. To use its functions you need to register.
-        </p>
+        <div>
+          <p>
+            This is a phone book. With which you can add and delete contacts. If
+            necessary you can search contacts by phone number or subscriber
+            name. To use its features, you need to <Link to="/register">register</Link> or <Link to="/login">log in</Link> using your
+            login (if you already have a registration).
+          </p>
+        </div>
       </PageTitle>
     </div>
   );
