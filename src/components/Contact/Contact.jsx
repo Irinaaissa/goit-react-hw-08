f3212024a7121086aa9cb29b190d97e60ec9a569
Irinaaissa <irinaalisa7506@gmail.com>
2024-04-12 21:09:47 +0300

    button

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,17 +3,12 @@ import { FaPhone } from "react-icons/fa6";
 import { IoPersonSharp } from "react-icons/io5";
 import { useState } from "react";
 import * as React from "react";
-// import Button from '@mui/material/Button';
 import Modal from "../Modal/Modal";
 
 export default function Contact({ item: { name, number, id } }) {
-  // const dispatch = useDispatch();
-  //
   const [isModalActive, setModalActive] = useState(false);
-  // const [userId,setUserId] = useState("");
-  //
+
   const handleModalOpen = () => {
-    // setUserId(id);
     setModalActive(true);
   };
   const handleModalClose = () => {
@@ -22,7 +17,7 @@ export default function Contact({ item: { name, number, id } }) {
 
   return (
     <div className={css.contactCard}>
-      <div>
+      <div className={css.contact}>
         <p className={css.info}>
           <span className={css.iconWrapper}>
             <IoPersonSharp />
@@ -35,23 +30,13 @@ export default function Contact({ item: { name, number, id } }) {
           </span>{" "}
           {number}
         </p>
-        
-        
-        
       </div>
-      <button className={css.btn} type="button" onClick={handleModalOpen}>
-  Delete
-</button>
-      {/* <button className="button" type="button" onClick={handleModalOpen}> */}
-      {/* open modal */}
-      {/* </button> */}
+      <div>
+        <button className={css.btn} type="button" onClick={handleModalOpen}>
+          Delete
+        </button>
+      </div>
 
-      {/* <button className={css.btn} type="button"> */}
-      {/* Edit */}
-      {/* </button> */}
-      {/* <button className={css.btn} onClick={() => dispatch(deleteContact(id))}> */}
-      {/* Delete */}
-      {/* </button> */}
       <div>
         {isModalActive && (
           <Modal handleModalClose={handleModalClose} userId={id} />
